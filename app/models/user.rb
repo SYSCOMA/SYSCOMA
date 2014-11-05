37a1d53c7fa0bb16a33ef09e252ebf87b8e28690
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2014-11-05 01:18:22 -0200

    Adding validation to name and username in forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :username, presence: true, length: { minimum: 4 }
 end
