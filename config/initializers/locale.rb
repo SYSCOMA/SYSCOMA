f9c45111d0542e2093b4fde6d4953928efe849f2
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2014-11-21 20:52:11 -0200

    Setting default language to English

---
+++ config/initializers/locale.rb
@@ -0,0 +1,6 @@
+# tell the I18n library where to find your translations
+# I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+ 
+# set default locale to something other than :en, like "pt-BR"
+# I18n.default_locale = :"pt-BR"
+I18n.default_locale = :en
